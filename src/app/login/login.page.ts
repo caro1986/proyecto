0da26aaf397013f6d28158253ec45d977f0acf7e
prueba <carolina.jimenez@pca.edu.co>
2022-07-28 22:52:55 -0500

    estilos de validators agregados y mostrar mensajes en el campo contraseña

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl,FormGroup, Validators } from '@angular/forms';
-import { NavController } from '@ionic/angular';
-import { AuthenticateService } from '../services/authenticates.service';
-import { Storage } from '@ionic/storage';
+import { FormBuilder, FormControl, FormGroup ,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,64 +7,45 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
- 
+
   loginForm: FormGroup;
   validation_messages = {
     email: [
-      { type: "require", message:"el email es obligatorio"},
-      { type: "pattern", message:"el email no es valido"}
+      { type: "required", message: "Este campo es obligatorio" },
+      { type: "pattern", message: "El email no es valido" }
     ],
-    password: [
-      { type: "require", message:"el password es obligatorio"},
-      { type: "pattern", message:"el password no es valido"}
-    ]
 
+    password:[
+      {type:"required", message:"Este campo es obligatorio"}, 
+      {type:"minlength", message:"Contraseña invalida"}
+    ]
   };
   errorMessage: any;
- 
-  constructor(private formBuilder: FormBuilder, 
-    private authService: AuthenticateService,
-    private navCtrl: NavController,
-    private storage: Storage) {
 
-    this.storage.create();
-      
+  constructor(private formBuilder: FormBuilder) { 
     this.loginForm = this.formBuilder.group({
-     
+
       email: new FormControl(
         "",
-        Validators.compose([
+        Validators.compose([  
           Validators.required,
           Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]+$")
         ])
       ),
+
       password: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(8)
-          
+          Validators.minLength(6)
         ])
-      )
+      ),
     })
-   }
+
+  }
+
 
   ngOnInit() {
-  }
-
-  loginUser(credentials){
-    this.authService.loginUser(credentials).then( res => {
-      this.errorMessage = ""; 
-      this.storage.set("isUserLoggendIn", true) 
-      this.navCtrl.navigateForward("/home");  
-    }).catch( err => {
-      this.errorMessage = err;
-    })
-  }
-
-  goToRegister(){
-    this.navCtrl.navigateForward("/resgister");
   }
 
 }
